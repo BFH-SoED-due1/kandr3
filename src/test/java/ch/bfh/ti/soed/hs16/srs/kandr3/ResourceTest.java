24106c6b908a577cc32d03512197620b143787f2
Eric Dubuis <eric.dubuis@bfh.ch>
2016-10-25 22:09:32 +0200

    Prepared for sprint 2. Perhaps needs to be merged with latest version.

--- src/test/java/ch/bfh/ti/soed/hs16/srs/kandr3/ResourceTest.java
+++ src/test/java/ch/bfh/ti/soed/hs16/srs/kandr3/ResourceTest.java
@@ -1,14 +1,12 @@
 package ch.bfh.ti.soed.hs16.srs.kandr3;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class ResourceTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 }
---
+++ src/test/java/ch/bfh/ti/soed/hs16/srs/kandr3/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016 Eric Dubuis, Berner Fachhochschule, Switzerland.
+ *
+ * Project Smart Reservation System.
+ *
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
+/**
+ * @author Eric Dubuis
+ */
+package ch.bfh.ti.soed.hs16.srs.kandr3;
